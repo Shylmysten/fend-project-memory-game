2dad401b1959ada6129d79de4d73d783658fe20c
Shylmysten <TBoswell@kc.rr.com>
2018-05-10 11:16:20 -0500

    Add event listener to Deck Element

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+/* DATA BLOCK */
+const gameData = {
+    openCards: [],
+    moves: 0,
+    starRating: 3
+}
+document.querySelector('.moves').innerText= gameData.moves;
+
 /*
  * Create a list that holds all of your cards
  */
-let cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 let deck = [];
 for (let i = 0; i < cards.length; i++) {
-    let card = cards[i];
+    const card = cards[i];
     deck.push(card);
 }
 
@@ -42,7 +50,7 @@ function buildNewCardTree(array) {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -55,13 +63,27 @@ function shuffle(array) {
     return array;
 }
 
+const cardClick = (event) => {
+
+
+
+}
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ 1. set up the event listener for a card. If a card is clicked:
+ */
+    UIDeck.addEventListener('click', cardClick);
+ /*  - display the card's symbol (put this functionality in another function that you call from this one) */
+
+
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) */
+
+ /*  - if the list already has another card, check to see if the two cards match */
+
+ /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) add match class and remove event addEventListener? */
+
+ /*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) */
+
+ /*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one) */
+
+ /*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
