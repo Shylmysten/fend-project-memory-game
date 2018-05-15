1a9cb0d1ad45f74806cd01d77368e89a4b0329ef
Shylmysten <TBoswell@kc.rr.com>
2018-05-14 20:21:56 -0500

    removed function winner, unified use of single quotes

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const gameData = {
   moves: 0,
   rank: 3,
   matchedCards: 0,
-  stars: document.querySelector(".stars"),
+  stars: document.querySelector('.stars'),
   seconds: 0,
   minutes: 0
 };
@@ -22,26 +22,26 @@ function init() {
   clearInterval(interval);
   gameData.seconds = 0;
   gameData.minutes = 0;
-  document.querySelector('#seconds').innerHTML = "00";
-  document.querySelector('#minutes').innerHTML = "00";
+  document.querySelector('#seconds').innerHTML = '00';
+  document.querySelector('#minutes').innerHTML = '00';
   // interval = setInterval(updateTimer, 1000);
   // reset cards matched back to 0
   gameData.matchedCards = 0;
   // set moves to 0 in dataBlock
   gameData.moves = 0;
   // set moves to 0 in UI
-  document.querySelector(".moves").innerText = gameData.moves + " Moves";
+  document.querySelector('.moves').innerText = gameData.moves + ' Moves';
   // begin the game to 3
   gameData.rank = 3;
   // clear the openCards list
   gameData.openCards = [];
   // Begin the game with 3 stars in the DOM
-  gameData.stars.innerHTML = `<li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>`;
+  gameData.stars.innerHTML = `<li><i class='fa fa-star'></i></li>
+                        <li><i class='fa fa-star'></i></li>
+                        <li><i class='fa fa-star'></i></li>`;
 
   // Create a list that holds all of your cards
-  const cards = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll('.card');
 
   // initialize the variable deck with an empty array
   let deck = [];
@@ -53,13 +53,13 @@ function init() {
 
   // Display the cards on the page
 
-  //   1. shuffle the list of cards using the provided "shuffle" method below
+  //   1. shuffle the list of cards using the provided 'shuffle' method below
   deck = shuffle(deck);
 
   //   2. Update UI with the new deck
 
   //  a. select deck in the UI
-  const UIDeck = document.querySelector(".deck");
+  const UIDeck = document.querySelector('.deck');
   //  b. remove all list items(cards) from the ul(deck) element in the UI
   while (UIDeck.firstChild) {
     UIDeck.removeChild(UIDeck.firstChild);
@@ -71,7 +71,7 @@ function init() {
   //  d. loop through each card and create its HTML
   for (let i = 0; i < deck.length; i++) {
     const newElement = deck[i];
-    newElement.setAttribute("class", "card");
+    newElement.setAttribute('class', 'card');
     // append the shuffled cards to the document fragment
     shuffledCards.appendChild(newElement);
   }
@@ -80,9 +80,9 @@ function init() {
   UIDeck.appendChild(shuffledCards);
 
   //  f. set up the event listener for a card. If a card is clicked:
-  UIDeck.addEventListener("click", cardClick);
+  UIDeck.addEventListener('click', cardClick);
   //  g. set up event listener for restart button
-  document.querySelector(".restart").addEventListener("click", init);
+  document.querySelector('.restart').addEventListener('click', init);
 } /* end of init function */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -104,14 +104,14 @@ function shuffle(array) {
 
 
 //@@@ the cardClick function is the main game engine that processes all USER
-//@ ACTIONS, updates the dataBlock and the UI to reflect those actions. Logic
+//@ ACTIONS. It updates the dataBlock and the UI to reflect those actions. Logic
 //@ for nearly all USER Actions are contained within, with exception to reset
 function cardClick(event) {
   // 1. is the target a card? is it already open, or matched?
   if (
-    !event.target.matches(".card") |
-    event.target.matches(".open") |
-    event.target.classList.contains("match")
+    !event.target.matches('.card') |
+    event.target.matches('.open') |
+    event.target.classList.contains('match')
   ) {
     // then we're not interested
     return;
@@ -123,14 +123,14 @@ function cardClick(event) {
 
   // 1. if the card isn't open
   // a. add open, show, flipInY, and animated classes
-  event.target.classList.add("animated", "open", "show", "flipInY");
+  event.target.classList.add('animated', 'open', 'show', 'flipInY');
   // b. add card(icon string/innerHTML?) to open list
   gameData.openCards.push(event.target);
   // c. add 1 to click count (to keep track of moves)
   gameData.moves++;
   // d. Update moves in UI
-  document.querySelector(".moves").innerText = gameData.moves + " Moves";
-  // e. check moveCount for Game Rank in "star"
+  document.querySelector('.moves').innerText = gameData.moves + ' Moves';
+  // e. check moveCount for Game Rank in 'star'
   if (
     (gameData.moves === 26) |
     (gameData.moves === 38) |
@@ -157,30 +157,30 @@ function cardClick(event) {
       gameData.matchedCards += 2;
 
       // iii. remove classes open, show, animated, flipInY
-      card1.classList.remove("open", "show", "flipInY");
-      card2.classList.remove("open", "show", "flipInY");
+      card1.classList.remove('open', 'show', 'flipInY');
+      card2.classList.remove('open', 'show', 'flipInY');
       // iv. add class rubberBand
-      card1.classList.add("rubberBand");
-      card2.classList.add("rubberBand");
+      card1.classList.add('rubberBand');
+      card2.classList.add('rubberBand');
       // v. add class match to both cards
-      card1.classList.add("match");
-      card2.classList.add("match");
+      card1.classList.add('match');
+      card2.classList.add('match');
     } else {
       // vi. if cards don't match
-      card1.classList.remove("flipInY");
-      card2.classList.remove("flipInY");
+      card1.classList.remove('flipInY');
+      card2.classList.remove('flipInY');
       // vii. Function wobble calls wobble but allows the animation to complete
       // before flipping the cards back over and removing the wobble class
       function wobble() {
-        // setTimeout adds the animation class "wobble" but allow a second to
+        // setTimeout adds the animation class 'wobble' but allow a second to
         // pass for the wobble animation to complete before removing classes
         // that would affect the animation
         setTimeout(function() {
-          card1.classList.remove("show", "open", "wobble");
-          card2.classList.remove("show", "open", "wobble");
+          card1.classList.remove('show', 'open', 'wobble');
+          card2.classList.remove('show', 'open', 'wobble');
         }, 1000);
-        card1.classList.add("wobble");
-        card2.classList.add("wobble");
+        card1.classList.add('wobble');
+        card2.classList.add('wobble');
       }
 
       wobble();
@@ -191,40 +191,48 @@ function cardClick(event) {
   }
   // 5. if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
   if (gameData.matchedCards === 16) {
-    winner();
+      // a. place the game timer in the variable timer before we clear it to
+      // display it in our modal
+      let time = document.querySelector('#minutes').innerText + ':' + document.querySelector('#seconds').innerText;
+      // b. stop the timer
+      clearInterval(interval);
+      // c. display the modal
+      const overlay = document.getElementById('overlay');
+      const modal = document.getElementById('winnerModal');
+      const sTime = document.getElementById('sTime');
+      const sRank = document.getElementById('sRank');
+      overlay.classList.toggle('showOverlay');
+      modal.classList.toggle('showModal');
+      sTime.innerText = time;
+      sRank.innerText = gameData.rank;
+      document.querySelector('#button-row').addEventListener('click', openModalResetGame);
   }
 } /* end of cardClick function */
 
-function winner() {
-  // a. place the game timer in the variable timer before we clear it to
-  // display it in our modal
-  let time = document.querySelector("#minutes").innerText + ":" + document.querySelector('#seconds').innerText;
-  // b. stop the timer
-  clearInterval(interval);
-  // c. display the modal
-  const overlay = document.getElementById('overlay');
-  const modal = document.getElementById('winnerModal');
-  const sTime = document.getElementById('sTime');
-  const sRank = document.getElementById('sRank');
-  overlay.classList.toggle('showOverlay');
-  modal.classList.toggle('showModal');
-  sTime.innerText = time;
-  sRank.innerText = gameData.rank;
-  document.querySelector('#button-row').addEventListener('click', openModalResetGame);
-
-}
-
+//@@ Function is called when the game is won to display a modal with the
+//@ User's current score and time. The modal also displays a 'no thanks' button
+//@ to close the modal and a 'Play again?' button that reinitializes the game.
 function openModalResetGame(event) {
+  // place the overlay element into the variable 'overlay'
   const overlay = document.getElementById('overlay');
+  // places the winnerModal element into the variable 'modal'
   const modal = document.getElementById('winnerModal');
+  // if the item clicked is not a button
   if (!event.target.matches('button')) {
+      // then we're not interested
       return;
   }
-  if (event.target.matches(".btn-close")) {
+  // if the item clicked was the close( No Thanks ) button
+  if (event.target.matches('.btn-close')) {
+    // remove the showOverlay and showModal classes to remove the modal from
+    // the users display
     overlay.classList.toggle('showOverlay');
     modal.classList.toggle('showModal');
   }
+  // if the item clicked was the reset ( Play Again? ) button
   if (event.target.matches('.btn-reset')) {
+      // remove the showOverlay and showModal classes to remove the modal from
+      // the users display and restart the game
       overlay.classList.toggle('showOverlay');
       modal.classList.toggle('showModal');
     init();
@@ -242,11 +250,11 @@ function updateTimer() {
   // results in the variable minutes
   let minutes = (gameData.minutes).toString();
 
-  // are there less than two characters in the string "seconds"
+  // are there less than two characters in the string 'seconds'
   if (seconds.length < 2) {
-    // if so, we need to prepend a "0" to the string in order for it to display
+    // if so, we need to prepend a '0' to the string in order for it to display
     // correctly
-    document.getElementById("seconds").innerHTML = "0" + seconds;
+    document.getElementById('seconds').innerHTML = '0' + seconds;
     // has the seconds counted reached 60?
   } else if (gameData.seconds === 60) {
     // if so, we need to reset both the seconds variable and the counted seconds
@@ -256,35 +264,23 @@ function updateTimer() {
     // passed
     ++gameData.minutes;
     // display the seconds in the UI and since we reset them to 0 we'll need to
-    // prepend a "0" in order for it to display correctly
-    document.getElementById("seconds").innerHTML = "0" + seconds;
+    // prepend a '0' in order for it to display correctly
+    document.getElementById('seconds').innerHTML = '0' + seconds;
     // are there less than 2 characters in the minutes string
     if (minutes.length < 2 ) {
-      // if so we need to prepend a "0" in order for it to display correctly in
+      // if so we need to prepend a '0' in order for it to display correctly in
       // the UI
-      document.getElementById("minutes").innerHTML = "0" + gameData.minutes;
+      document.getElementById('minutes').innerHTML = '0' + gameData.minutes;
     } else {
       // if there are more than 2 characters, then just display the minutes in
       // the UI
-      document.getElementById("minutes").innerHTML = gameData.minutes;
+      document.getElementById('minutes').innerHTML = gameData.minutes;
     }
     // if there are more than 2 characters in the seconds variable
   } else {
     // then display the seconds in the UI
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById('seconds').innerHTML = seconds;
   }
 
 
 }
-
-/*  - display the card's symbol (put this functionality in another function that you call from this one) */
-
-/*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) */
-
-/*  - if the list already has another card, check to see if the two cards match */
-
-/*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) add match class and remove event addEventListener? */
-
-/*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) */
-
-/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one) */
